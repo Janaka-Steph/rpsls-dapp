5261968d6d4606e4c4164a47b84575c9940bb5bf
Janaka-Steph <rochestephane@yahoo.fr>
2017-08-30 15:57:41 +0200

    config prod fuse

--- fuse.js
+++ fuse.js
@@ -3,7 +3,6 @@ const express = require('express')
 const resolveId = require('postcss-import/lib/resolve-id')
 const path = require('path')
 const isProduction = process.env.NODE_ENV === 'production' ? true : false
-
 const POSTCSS_PLUGINS = [
   require('postcss-import')({
     root: path.join(__dirname, 'src', 'ui'),
@@ -13,68 +12,80 @@ const POSTCSS_PLUGINS = [
     browsers: ['ie >= 11', 'last 2 versions'],
   }),
 ]
-
-Sparky.task('default', ['clean', 'copy-assets', 'build'], () => {})
-Sparky.task('clean', () => Sparky.src(path.resolve('build')).clean(path.resolve('build')))
+// const outputName = isProduction ? '$name.min.js' : '$name.js'
+// Producer
+const fuse = FuseBox.init({
+  alias: {
+    'reactstrap-tether': '', // todo Remove this hack to fix import of reactstrap-tether
+    '../../customModules/protocol/index.js': 'protocol/index.js', // hack to have working path for app and tests
+    '../../customModules/protocol/truffle.js': 'protocol/truffle.js',
+  },
+  cache: !isProduction,
+  debug: true,
+  experimentalFeatures: true, // remove next major release of fb
+  homeDir: 'src',
+  //ignoreModules : [],
+  log: true,
+  modulesFolder: 'customModules',
+  //output: `build/${outputName}`,
+  output: 'build/$name.js',
+  plugins: [
+    EnvPlugin({
+      NETWORK: process.env.NETWORK || 'testrpc',
+      NODE_ENV: isProduction ? 'production' : 'development',
+    }),
+    [/components.*\.css$/, PostCSSPlugin(POSTCSS_PLUGINS), CSSModules(), CSSPlugin()],
+    [PostCSSPlugin(POSTCSS_PLUGINS), CSSResourcePlugin({
+      dist: 'build/assets',
+      resolve: f => `/assets/${f}`,
+    }), CSSPlugin()],
+    JSONPlugin(),
+    WebIndexPlugin({
+      //bundles: ['assets/bundle.min.js'],
+      template: 'src/ui/index.html',
+      path: '.',
+    }),
+    isProduction && QuantumPlugin({
+      api: (core) => {
+        core.solveComputed('bn.js/lib/bn.js') //todo Remove when BN fix it
+      },
+      bakeApiIntoBundle: 'assets/bundle.min.js',
+      ensureES5: true,
+      removeExportsInterop: false,
+      target: 'universal',
+      treeshake: true,
+      uglify: true,
+    }),
+  ],
+  sourceMaps: !isProduction,
+  // target: 'browser',
+  useJsNext: false,
+})
+// Tasks
 Sparky.task('clean-cache', () => Sparky.src('.fusebox/*').clean('.fusebox/'))
+Sparky.task('default', ['clean', 'copy-assets', 'run'], () => {})
+Sparky.task('clean', () => Sparky.src(path.resolve('build')).clean(path.resolve('build')))
 Sparky.task('copy-assets', () => Sparky.src('assets/**/**.*', {base: './src/ui'}).dest('build'))
-Sparky.task('build', () => {
-  const fuse = FuseBox.init({
-    alias: {
-      "reactstrap-tether": '', // todo Remove this hack to fix import of reactstrap-tether
-      "../../customModules/protocol/index.js": "protocol/index.js", // hack to have working path for app and tests
-      "../../customModules/protocol/truffle.js": "protocol/truffle.js"
-    },
-    cache: !isProduction,
-    debug: true,
-    experimentalFeatures: true, // remove next major release of fb
-    homeDir: 'src',
-    //ignoreModules : [],
-    log: true,
-    modulesFolder: 'customModules',
-    output: 'build/$name.js',
-    plugins: [
-      EnvPlugin({
-        NETWORK: process.env.NETWORK || 'testrpc',
-        NODE_ENV: isProduction ? 'production' : 'development',
-      }),
-      [/components.*\.css$/, PostCSSPlugin(POSTCSS_PLUGINS), CSSModules(), CSSPlugin()],
-      [PostCSSPlugin(POSTCSS_PLUGINS), CSSResourcePlugin({
-        dist: 'build/assets',
-        resolve: f => `/assets/${f}`,
-      }), CSSPlugin()],
-      JSONPlugin(),
-      WebIndexPlugin({
-        template: 'src/ui/index.html',
-        path: '.',
-      }),
-      isProduction && QuantumPlugin({
-        api: (core) => {
-          core.solveComputed("bn.js/lib/bn.js") //todo Remove when BN fix it
-        },
-        bakeApiIntoBundle: 'assets/app',
-        ensureES5: true,
-        removeExportsInterop: false,
-        treeshake: true,
-        uglify: true,
-      }),
-    ],
-    sourceMaps: !isProduction,
-    target: 'browser',
-    useJsNext: false,
-  })
-
-  !isProduction && fuse.dev({open: false, port: 8085, root: 'build'}, server => {
-    const build = path.resolve('build')
-    const app = server.httpServer.app
-    app.use('/assets/', express.static(path.join(build, 'assets')))
-    app.get('*', function (req, res) {
-      res.sendFile(path.resolve('build', 'index.html'))
+Sparky.task('run', () => {
+  if (isProduction) {
+    fuse.bundle('assets/bundle.min.js')
+      .watch('server/**') // watch only server related code
+      .instructions('> [../server.ts]')
+      // Execute process right after bundling is completed
+      // launch and restart express
+      .completed(proc => proc.start())
+  }
+  else {
+    fuse.dev({open: false, port: 8085, root: 'build'}, server => {
+      const app = server.httpServer.app
+      app.use('/assets/', express.static(path.resolve('build', 'assets')))
+      app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
     })
-  })
-
-  const app = fuse.bundle('assets/app')
-    .instructions(`>ui/index.tsx`)
-  !isProduction && app.hmr().watch()
+    fuse.bundle('assets/app')
+      .target('browser')
+      .instructions(`>ui/index.tsx`)
+      .hmr()
+      .watch()
+  }
   fuse.run()
 })
